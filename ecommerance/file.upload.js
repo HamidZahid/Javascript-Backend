03ae4f888995606aede07d303926b5654907ccbb
HamidZahid <hamidchaudhary3867@gmail.com>
2024-07-30 17:33:21 +0500

    add

--- ecommerance/file.upload.js
+++ ecommerance/file.upload.js
@@ -1,25 +1,25 @@
-import {v2 as cloudinary} from "cloudinary"
-import fs from "fs"
+// import {v2 as cloudinary} from "cloudinary"
+// import fs from "fs"
 
-cloudinary.config(
-    {
-        cloud_name: cloudinary,
-        api_key: "3294829847823293",
-        api_secret:"podjoisjiOJIUOHJwiojis"
-    }
-)
+// cloudinary.config(
+//     {
+//         cloud_name: cloudinary,
+//         api_key: "3294829847823293",
+//         api_secret:"podjoisjiOJIUOHJwiojis"
+//     }
+// )
 
-const uploadfiletoCloudinary = async (localfilepath) =>{
-    try {
-       if(!localfilepath) return null
-        // if file uploaded then 
-        const responce = await cloudinary.uploader.upload(localfilepath, {
-            resource_type: "auto"
-        }) 
-        console.log("File Uploaded Successfully", responce.url)
-        return responce;
-    } catch (error) {
-        fs.unlinkSync(localfilepath)
-        return null
-    }
-}
+// const uploadfiletoCloudinary = async (localfilepath) =>{
+//     try {
+//        if(!localfilepath) return null
+//         // if file uploaded then 
+//         const responce = await cloudinary.uploader.upload(localfilepath, {
+//             resource_type: "auto"
+//         }) 
+//         console.log("File Uploaded Successfully", responce.url)
+//         return responce;
+//     } catch (error) {
+//         fs.unlinkSync(localfilepath)
+//         return null
+//     }
+// }
