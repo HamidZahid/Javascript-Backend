4f9b688ab0e82ce8bc79f8522710a29a9f558c07
HamidZahid <hamidchaudhary3867@gmail.com>
2024-07-30 14:31:55 +0500

    File Update

--- ecommerance/product.models.js
+++ ecommerance/product.models.js
@@ -1,4 +1,17 @@
 import mongoose from "mongoose";
+import jsonwebtoken from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const ConnectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`\n MOngoDb Connected !! DB HOst ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("MongoDB Connection Failed", error)
+        process.exit(1)
+    }
+}
+
 
 const ProductidSchema = new mongoose.Schema({
     productif:{
@@ -22,83 +35,25 @@ export const Product = mongoose.model("Product", productSchema);
 
 
 
-const ConnectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-        console.log(`\n MOngoDb Connected !! DB HOst ${connectionInstance.connection.host}`)
-    } catch (error) {
-        console.log("MongoDB Connection Failed", error)
-        process.exit(1)
-    }
-}
 
+
+// productSchema.pre("save", async funtion (next) {
+//     this.password = bcrypt.hash(this.password, 10)
+//     next()
+// })
+
+productSchema.methods.gerenateAccesstoken = function () {
+    jwt.sign({
+        _id: this.id,
+        name: this.name,
+        email: this.email,
+        fullname: this.fullname
+    },
+    process.env.tokenname,
+    {
+        expireIn: process.env.expirevariablename
+    }
+)
+}
 export default ConnectDB
-
-
-const connectDB = async () =>{
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.Databaseurl}/${wheretoexport}`)
-        console.log(`MongoDB Connected !! DB Host ${connectionInstance.connection.host}`)
-    } catch (error) {
-        console.log("Connection Failed", error)
-        process.exit(1)
-    }
-}
-
-
-
-
-const ConnectionDBs = async () => {
-    try {
-
-        const ConnectionsInstance = await mongoose.connection(`${process.env.DATABASE_URL}/${DB_NAMES}`)
-        console.log(`MONGODB Connected !! DB HOST ${connectionInstance.connection.host}`)
-        
-    } catch (error) {
-        console.log("DataBase connection Failed", error)
-        process.exit(1)
-        
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
